58b61c8f5950ee9e2838df9164bbc66667dcc627
JR <178076675+JustRelaXD@users.noreply.github.com>
2025-09-04 22:35:21 +0530

    added inital_db

--- app.py
+++ app.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 
 import sqlite3
 
+def inital_db():
+    conn = sqlite3.connect("tasks.db")
+    c = conn.cursor()
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            task TEXT NOT NULL,
+            task_date TEXT NOT NULL,
+            task_time TEXT NOT NULL
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+initial_db() 
 
 
