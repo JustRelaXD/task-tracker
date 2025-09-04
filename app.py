bf16fb42b5308b6827432fa0dd7e81dac029fb32
JR <178076675+JustRelaXD@users.noreply.github.com>
2025-09-04 22:30:23 +0530

    update flask import

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template_string, request, redirect
+import sqlite3
+from datetime import date
 
 app = Flask(__name__)
 
 import sqlite3
+
+
 
